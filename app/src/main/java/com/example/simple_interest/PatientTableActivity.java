5069143db306becd0f333ba4358be370f5de68db
Sudeeppudasaini <heroclan82@gmail.com>
2025-05-12 22:44:52 +0545

    new new

---
+++ app/src/main/java/com/example/simple_interest/PatientTableActivity.java
@@ -0,0 +1,14 @@
+package com.example.simple_interest;
+
+//package com.example.patientregistration; // <-- Change according to your package name
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientTableActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_table);
+    }
+}
