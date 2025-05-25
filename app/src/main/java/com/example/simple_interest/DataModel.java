23255ff5a4e7ea4204a4d25e2c82bc5838968db4
Sudeeppudasaini <heroclan82@gmail.com>
2025-05-25 23:04:39 +0545

    add

--- app/src/main/java/com/example/simple_interest/DataModel.java
+++ app/src/main/java/com/example/simple_interest/DataModel.java
@@ -11,7 +11,7 @@ public class DataModel {
         this.address = address;
     }
 
-
+    public int getId() { return id; }
     public String getName() { return name; }
     public String getAddress() { return address; }
 }
