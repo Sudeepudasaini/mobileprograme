f9b8980eefce014a415e973fe0881b7c8c543ce7
Sudeeppudasaini <heroclan82@gmail.com>
2025-06-15 21:35:24 +0545

    new

--- app/src/main/java/com/example/simple_interest/DataModel.java
+++ app/src/main/java/com/example/simple_interest/DataModel.java
@@ -7,7 +7,7 @@ public class DataModel {
 
     public DataModel(int id, String name, String address) {
         this.id = id;
-        this.name = name;
+        
         this.address = address;
     }
 
